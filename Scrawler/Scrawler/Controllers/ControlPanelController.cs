6e1fae2b7c6e83eef8f3d6426a8b643563461b50
regan-young <im.regan.young@gmail.com>
2014-07-04 11:35:39 +1200

    Deploying to test SQL connection

---
+++ Scrawler/Scrawler/Controllers/ControlPanelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scrawler.Plumbing;
+using Scrawler.Plumbing.Interfaces;
+
+namespace Scrawler.Controllers
+{
+    public class ControlPanelController : ScrawlerController
+    {
+        private readonly IRepository<Chatroom> _chatRepository;
+
+
+        public ActionResult Index()
+        {
+            var listofChatrooms = _chatRepository.GetAll();
+
+            return View(listofChatrooms);
+        }
+
+        public ActionResult AddRoom()
+        {
+            return View();
+        }
+    }
+}
