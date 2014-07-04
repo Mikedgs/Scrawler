cf5fb207a5acca43954bb3dff81ee58d0c7f250c
regan-young <im.regan.young@gmail.com>
2014-07-04 14:01:42 +1200

    Added easy control panel for testing

--- Scrawler/Scrawler/Controllers/ControlPanelController.cs
+++ Scrawler/Scrawler/Controllers/ControlPanelController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Scrawler.Models;
+using Scrawler.Models.Services;
 using Scrawler.Plumbing;
 using Scrawler.Plumbing.Interfaces;
 
@@ -10,19 +12,44 @@ namespace Scrawler.Controllers
 {
     public class ControlPanelController : ScrawlerController
     {
-        private readonly IRepository<Chatroom> _chatRepository;
-
+        private readonly IRepository<Chatroom> _chatRepository = new Repository<Chatroom>();
+        private IHiddenStringFactory _stringFactory = new HiddenStringFactory();
 
         public ActionResult Index()
         {
+
             var listofChatrooms = _chatRepository.GetAll();
 
             return View(listofChatrooms);
         }
 
+        [HttpGet]
         public ActionResult AddRoom()
         {
-            return View();
+            var room = new Chatroom();
+            return View(room);
+        }
+
+        [HttpPost]
+        public ActionResult AddRoom(Chatroom room)
+        {
+            room.HiddenUrl = _stringFactory.GenerateHiddenString();
+            room.CreatedAt = DateTime.Now;
+
+            _chatRepository.Add(room);
+            _chatRepository.SaveChanges();
+
+            return Redirect("/ControlPanel/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var room = _chatRepository.FindById(id);
+            _chatRepository.Delete(room);
+            _chatRepository.SaveChanges();
+
+            return Redirect("/ControlPanel/Index");
         }
     }
 }
